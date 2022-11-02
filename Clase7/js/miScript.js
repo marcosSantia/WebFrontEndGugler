6a5e3e5bd6c3ac5f8c6ae76d1bc567fb009b9f11
marcosSantia <maycolsantia@gmail.com>
2022-11-02 03:43:14 -0300

    practico 7

--- Clase7/js/miScript.js
+++ Clase7/js/miScript.js
@@ -1,1 +1,34 @@
-console.log("Hola Mundo");
+console.log("Bienvenido al lenguaje javascript");
+
+console.log("Bienvenido al lenguaje de javascript \njs es un lenguaje de programacion que se ejecuta de lado del cliente");
+
+let numero = 10;
+
+let objeto = {
+    nombre: "Juan",
+    apellido: "Perez",
+}
+
+let string = "Hola mundo";
+
+let booleano = true;
+
+console.log(numero);
+console.log(objeto);
+console.log(string);
+console.log(booleano);
+
+console.log(typeof numero);
+console.log(typeof(objeto));
+console.log(typeof(string));
+console.log(typeof(booleano));
+
+
+let numero1 = 10;
+let numero2 = 20;
+
+console.log(numero1 + numero2);/* suma */
+console.log(numero1 - numero2);
+console.log(numero1 * numero2);
+console.log(numero1 / numero2);
+console.log(numero1 % numero2);
